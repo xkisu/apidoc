25bb5db66d14defd461b4f9d68ff03bb745a657d
Keith Mitchell <keith.mitchell@live.ca>
2018-01-24 20:04:45 -0800

    Starting code

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # apidoc
-Platform for writing API specifications, code, and documentation all in one
+
+This is a work in progress! At this point, it is a concept with some rough code.
+
+Platform for writing API specifications, code, and documentation all in one file. 
+
+Write a single JSON file to generate API docs and easily create Express routes on the server side, and call the routes from the client side without having to repeat your route paths everywhere.
+
+The doc should allow for a wide range of configuration, including things like automatically including access token headers on certain routes without the app needed to set it on the required routes manually. 
+
+Example Express code:
+```JavaScript
+const APIDoc = require('apidoc')
+const api = new APIDoc('./mydoc.json')
+
+const express = require('express')
+const app = express()
+
+// #apidoc.express() creates an express router, handles validation for parameters, exposes parameters and any other required data, and then passess it to the standard callback structure for express
+app.use(api.express('myRouteName', function(req, res){
+    const name = req.api.param.name;
+}))
+```
+
+Example Frontend code: 
+```JavaScript
+API.init('https://myapidoc');
+
+API.call('myRouteName', {
+    parameters: {
+        'name': 'Kisu Mitieru'
+    }
+})
+```
---
+++ src/module/index.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const Buffer = require('buffer').Buffer;
+const request = require('request');
+
+const defaultOpts = {
+    encoding: 'utf8'
+}
+
+class API {
+    constructor (doc, opts) {
+
+        this.opts = Object.assign({}, defaultOpts, this.opts)
+
+        if(typeof doc == "string") {
+            var urlRegex = /^(http|https)/;
+            var jsonRegex = /^\{/;
+            if(urlRegex.test(doc)) {
+                request({
+                    method: 'GET',
+                    uri: doc,
+                    preambleCRLF: false,
+                    postambleCRLF: false,
+                },
+                function (error, response, body) {
+                    if (error) {
+                        throw new Error('Error while retreving api doc: ' + error);
+                    } 
+                    if(typeof body == 'string' || jsonRegex.test(body)) {
+                        this.doc = JSON.parse(body);
+                    } else if (doc === Object(doc) && Object.prototype.toString.call(doc) !== '[object Array]') {
+                        this.doc = body;
+                    } else {
+                        throw new Error('Unknown formatted response to url fetch for api doc');
+                    }
+                })
+            } else if (fs.existsSync(doc)) {
+                this.doc = fs.readFileSync(doc, {
+                    encoding: this.opts.encoding
+                });
+            } else if (jsonRegex.test(doc)) {
+                this.doc = JSON.parse(doc);
+            } else {
+                throw new Error("First parameter is not a url, json object, or file path.");
+            }
+        } else if (doc instanceof Buffer) {
+            this.doc = doc.toString(this.opts.encoding);
+        } else if (doc === Object(doc) && Object.prototype.toString.call(doc) !== '[object Array]') {
+            this.doc = doc;
+        } else {
+            throw new Error("First parameter is not a url, json object, file path, or buffer.");
+        }
+    }
+}
+
+module.exports = API;
