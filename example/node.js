d08a81d338d4a59d383461c5c515f2e72c8f2301
Keith Mitchell <keith.mitchell@live.ca>
2018-01-27 23:20:30 -0800

    Started adding express intergration and figuring out doc schema

--- example/node.js
+++ example/node.js
@@ -3,11 +3,15 @@ const api = new APIDoc('./apidoc.json');
 
 const express = require('express');
 const app = express();
-/*
-app.use(api.express('root', function (req, res) {
 
+app.use(api.express('root', function (req, res) {
+    res.send('hi');
 }));
-*/
+
+app.use(api.express('params', function (req, res) {
+    res.send('hi');
+}));
+
 app.listen(3000, function(){
     console.log('Example ready on http://localhost:3000');
 })
